dfc6aad310f113bcdd95f829690d00d2d91c3644
mogyorosibalin <mogyorosibalin@gmail.com>
2018-06-22 15:48:48 +0200

    FeaturedProductCategory implemented totally

--- src/com/codecool/uml/overloading/FeaturedProductCategory.java
+++ src/com/codecool/uml/overloading/FeaturedProductCategory.java
@@ -1,5 +1,36 @@
 package com.codecool.uml.overloading;
 
-public class FeaturedProductCategory extends ProductCategory {
+import java.util.Date;
 
+public class FeaturedProductCategory extends ProductCategory {
+    Date startDate;
+    Date endDate;
+
+    void schedule(Date startDate, Date endDate) {
+        setStartDate(startDate);
+        setEndDate(endDate);
+    }
+
+    void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    Date getStartDate() {
+        return startDate;
+    }
+
+    void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    Date getEndDate() {
+        return endDate;
+    }
+
+    public String toString() {
+        return String.format(
+                "FeaturedProductCategory::id:%s,name:%s,department:%s,description:%s,startDate:%s,endDate:%s",
+                getId(), getName(), getDepartment(), getDescription(), getStartDate().toString(), getEndDate().toString()
+        );
+    }
 }
