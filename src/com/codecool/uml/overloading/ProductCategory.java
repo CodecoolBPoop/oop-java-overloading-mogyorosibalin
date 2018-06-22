adff95e1292bc7a5a7aac1623cf3f274c8978816
mogyorosibalin <mogyorosibalin@gmail.com>
2018-06-22 15:41:01 +0200

    ProductCategory: constructors and setter getters updated

--- src/com/codecool/uml/overloading/ProductCategory.java
+++ src/com/codecool/uml/overloading/ProductCategory.java
@@ -1,5 +1,29 @@
 package com.codecool.uml.overloading;
 
 public class ProductCategory extends Base {
+    static int productCategoryCount = 0;
 
+    String department;
+
+    ProductCategory() {
+        setId(productCategoryCount++);
+        setName("");
+        setDepartment("");
+        setDescription("");
+    }
+
+    ProductCategory(String name, String department, String description) {
+        setId(productCategoryCount++);
+        setName(name);
+        setDepartment(department);
+        setDescription(description);
+    }
+
+    void setDepartment(String department) {
+        this.department = department;
+    }
+
+    String getDepartment() {
+        return department;
+    }
 }
