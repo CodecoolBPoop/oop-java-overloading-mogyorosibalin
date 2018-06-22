a6fa96f1efee457854c59628593d6f12294439b5
mogyorosibalin <mogyorosibalin@gmail.com>
2018-06-22 15:34:48 +0200

    Supplier: constructor methods implemented

--- src/com/codecool/uml/overloading/Supplier.java
+++ src/com/codecool/uml/overloading/Supplier.java
@@ -1,5 +1,18 @@
 package com.codecool.uml.overloading;
 
 public class Supplier extends Base {
+    static int supplierCount = 0;
+
+    Supplier() {
+        setId(supplierCount++);
+        setName("");
+        setDescription("");
+    }
+
+    Supplier(String name, String description) {
+        setId(supplierCount++);
+        setName(name);
+        setDescription(description);
+    }
 
 }
