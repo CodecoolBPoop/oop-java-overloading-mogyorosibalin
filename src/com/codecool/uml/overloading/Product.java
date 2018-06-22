95b2a7194331fc26cc5107e2e9185ea0da72df53
mogyorosibalin <mogyorosibalin@gmail.com>
2018-06-22 14:57:03 +0200

    Product: basic constructors added

--- src/com/codecool/uml/overloading/Product.java
+++ src/com/codecool/uml/overloading/Product.java
@@ -3,6 +3,7 @@ package com.codecool.uml.overloading;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Currency;
+import java.util.Locale;
 
 public class Product extends Base {
     static int itemCount = 0;
@@ -12,5 +13,25 @@ public class Product extends Base {
     ProductCategory productCategory;
     Supplier supplier;
     List<Product> productList = new ArrayList<>();
+
+    Product() {
+        setName("");
+        setDefaultPrice(0);
+        setDefaultCurrency(Currency.getInstance(Locale.US));
+    }
+
+    Product(String name, float defaultPrice, Currency defaultCurrency) {
+        setName(name);
+        setDefaultPrice(defaultPrice);
+        setDefaultCurrency(defaultCurrency);
+    }
+
+    void setDefaultPrice(float defaultPrice) {
+        this.defaultPrice = defaultPrice;
+    }
+
+    void setDefaultCurrency(Currency defaultCurrency) {
+        this.defaultCurrency = defaultCurrency;
+    }
 
 }
